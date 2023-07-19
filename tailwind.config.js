d198f0b90b55eb64df4da0544a1e47020acdcc44
Sivritkin Dmitriy <dmitriysivritkin@gmail.com>
2023-07-19 22:42:17 +0300

    add: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,20 @@
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    colors: {
-      white: '#fff',
-      black: '#000',
-      secondary: '#ccc',
-      primary: '#111',
-      grey: '#757575'
-    },
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ['Helvetica', 'Arial', 'sans-serif'],
+        nike: ['Nike', 'sans-serif'],
+        sono: ['Sono', 'sans-serif']
+      },
+      colors: {
+        white: '#fff',
+        secondary: '#ccc',
+        primary: '#111',
+        grey: '#757575',
+        greyLight: '#e5e5e5'
+      }
+    }
   },
   plugins: []
 };
